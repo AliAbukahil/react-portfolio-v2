1e95ee1bce5f06de82884d94abec1a4898fc58a7
ali.abukahil <ali.abukahil@gmail.com>
2021-07-05 14:25:05 +0200

    add travelly and Elana joy

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,37 +13,30 @@ import youtube from "../icons/youtube-symbol 1.svg";
 const Projects = () => {
   return (
     <div>
-      {/*       <header className="main-head">
-        <nav>
-          <div className="logo">
-            <img src="../icons/code.svg" alt="logo" />
-            <h1>Dev ALI</h1>
-          </div>
-          <ul>
-            <li>
-              <Link to="../index.html">Home</Link>
-            </li>
-            <li>
-              <Link className="active" to="./index.html">
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link to="../contact/index.html">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header> */}
       <section className="projects">
         <div className="travelly">
           <h2>Travelly</h2>
-          <Link to="/">
+          <Link to="https://aliabukahil.github.io/travel-WebSite/">
             <img src={travel} alt="travelly-project" />
           </Link>
         </div>
         <div className="elena">
           <h2>Elena Joy</h2>
-          <Link to="/">
+          <Link to="https://aliabukahil.github.io/photographyWebsite-Elena-Joy/">
+            <img src={elenaJoy} alt="elena-joy-project" />
+          </Link>
+        </div>
+      </section>
+      <section className="projects">
+        <div className="travelly">
+          <h2>Travelly</h2>
+          <Link to="https://aliabukahil.github.io/travel-WebSite/">
+            <img src={travel} alt="travelly-project" />
+          </Link>
+        </div>
+        <div className="elena">
+          <h2>Elena Joy</h2>
+          <Link to="https://aliabukahil.github.io/photographyWebsite-Elena-Joy/">
             <img src={elenaJoy} alt="elena-joy-project" />
           </Link>
         </div>
